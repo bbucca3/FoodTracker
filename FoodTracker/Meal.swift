188d00062f50eef72382c7334b21632d305cb692
Benjamin Bucca <benjamin.bucca3@gmail.com>
2018-04-03 18:52:00 -0700

    Define Data Model

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,3 +7,28 @@
 //
 
 import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusive
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        // Initialize stored properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
